0b96ef5aa44867b1be957a396c9c31a20a91ec63
Nicholas Bryan <njb@ccrma.stanford.edu>
2013-12-15 13:12:13 -0800

    update juce library code

--- JuceLibraryCode/modules/juce_gui_basics/juce_gui_basics.h
+++ JuceLibraryCode/modules/juce_gui_basics/juce_gui_basics.h
@@ -1,5 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../SDKs/juce/modules/juce_gui_basics/juce_gui_basics.h"
+#include "../../../sdks/juce/modules/juce_gui_basics/juce_gui_basics.h"
 
